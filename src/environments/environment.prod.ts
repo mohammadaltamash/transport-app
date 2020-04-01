6e20e08bc0293155358053e7c98ca584c020c95e
Muhammad Altamash <muhammad.altamesh@gmail.com>
2020-04-01 20:21:53 +0500

    Revert

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,6 +2,7 @@ export const environment = {
   production: true,
 
   REST_SERVICE_URL: 'https://transport-app-rest.herokuapp.com/transportapp',
+  // REST_SERVICE: 'http://localhost:8080/transportapp',
   // https://account.smartystreets.com/#keys
   SS_AUTH_ID: '47f66a5f-f9f4-be57-fad2-8aa1c1a6c4ad',
   SS_AUTH_TOKEN: 'aIw6eGhZorEhYLQnQySb',
@@ -9,8 +10,5 @@ export const environment = {
   US_ZIP_VALIDATOR_URL: 'https://us-zipcode.api.smartystreets.com/lookup',
 
   NEW_ORDER: 'NEW',
-  ASSIGNED_ORDER: 'ASSIGNED',
-  ACCEPTED_ORDER: 'ACCEPTED',
-
-  USER_DRIVER: 'DRIVER'
+  ACCEPTED_ORDER: 'ACCEPTED'
 };
