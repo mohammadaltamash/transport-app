f77acbc87c8354b138ec7e0ffab6a1634a7247e9
Muhammad Altamash <muhammad.altamesh@gmail.com>
2020-05-02 02:32:10 +0500

    Update

--- src/app/component/module/manage-orders/book-order/book-order.component.ts
+++ src/app/component/module/manage-orders/book-order/book-order.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-book-order',
   templateUrl: './book-order.component.html',
-  styleUrls: ['./book-order.component.css']
+  styleUrls: ['./book-order.component.scss']
 })
 export class BookOrderComponent implements OnInit {
 
