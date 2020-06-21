cf367ce799b5c0fb8cd25d2e6370b77167d813cd
Muhammad Altamash <muhammad.altamesh@gmail.com>
2020-06-21 18:22:44 +0500

    Update

--- src/app/component/module/manage-orders/create-company-dialog/create-company-dialog.component.ts
+++ src/app/component/module/manage-orders/create-company-dialog/create-company-dialog.component.ts
@@ -5,6 +5,9 @@ import { ApiService } from 'src/app/service/api.service';
 import { AuthenticationService } from 'src/app/service/authentication.service';
 import { Utilities } from 'src/app/helper/utilities';
 import { Company } from 'src/app/model/company';
+import { ErrorHandler } from '../../../../helper/error_handler';
+import PlaceResult = google.maps.places.PlaceResult;
+import { Location } from '@angular-material-extensions/google-maps-autocomplete';
 
 @Component({
   selector: 'app-create-company-dialog',
@@ -13,6 +16,10 @@ import { Company } from 'src/app/model/company';
 })
 export class CreateCompanyDialogComponent implements OnInit {
   companyForm: FormGroup;
+  country = 'us';
+  address: string;
+  latitude: number;
+  longitude: number;
 
   constructor(
     private dialogRef: MatDialogRef<CreateCompanyDialogComponent>,
@@ -20,7 +27,8 @@ export class CreateCompanyDialogComponent implements OnInit {
     private formBuilder: FormBuilder,
     private apiService: ApiService,
     private authenticationService: AuthenticationService,
-    private utilities: Utilities
+    private utilities: Utilities,
+    public errorHandler: ErrorHandler
   ) {}
 
   ngOnInit(): void {
@@ -39,6 +47,9 @@ export class CreateCompanyDialogComponent implements OnInit {
 
   onSubmit() {
     const company: Company = this.companyForm.value;
+    company.address = this.address;
+    company.latitude = this.latitude;
+    company.longitude = this.longitude;
     console.log(company);
     this.apiService.createCompany(company).subscribe(comp => {
       this.utilities.showSuccess('Company added', 'Company');
@@ -48,6 +59,53 @@ export class CreateCompanyDialogComponent implements OnInit {
 
   onCloseClick() {
     this.dialogRef.close({ created: false });
+  }
+
+  get formControls() {
+    return this.companyForm.controls;
+  }
+
+  // Address
+
+  onAutocompleteSelected(result: PlaceResult) {
+    console.log('onAutocompleteSelected: ', result);
+    // const postalCode = this.getPostalCode(result);
+    // console.log(postalCode);
+    // this.addressIsValid = true;
+    // const place = autocomplete.getPlace();
+    const address_components = result.address_components;
+    const postalCode = this.extractFromAddress(
+      address_components,
+      'postal_code'
+    );
+
+    this.address = result.formatted_address;
+    this.formControls.zip.setValue('');
+    if (postalCode !== null) {
+      this.formControls.zip.setValue(Number(postalCode));
+    }
+  }
+
+  extractFromAddress(
+    components: google.maps.GeocoderAddressComponent[],
+    type: string
+  ) {
+    return (
+      components
+        .filter(component => component.types.indexOf(type) === 0)
+        .map(item => item.long_name)
+        .pop() || null
+    );
+  }
+
+  onLocationSelected(location: Location) {
+    console.log('onLocationSelected: ', location);
+    const latitude: number = location.latitude;
+    const longitude: number = location.longitude;
+    this.latitude = latitude;
+    this.longitude = longitude;
+    // this.verifyAddress(zipCategory);
+    // console.log(this.validationResult);
   }
 
   // errorHandling(control: string, error: string) {
